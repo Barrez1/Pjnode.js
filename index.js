c5c20acb3bb9524b4d14212800b0fd07043f9434
Barrez1 <barresepedro18@gmail.com>
2023-06-06 21:17:42 -0300

    criando integracao bd

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const Post = require('./models/Post')
+const { where } = require('sequelize')
 
 // config 
 
 
 //Template engine
-app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars.engine({ 
+    defaultLayout: 'main', 
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true, 
+    },    
+}))
 app.set('view engine', 'handlebars')
 
     //body parser 
@@ -15,12 +22,37 @@ app.set('view engine', 'handlebars')
     app.use(express.json())
 
         //rota
+        app.get('/', function(req, res){
+            Post.findAll({order: [['id','DESC']]}).then(function(posts){
+                console.log(posts)
+                res.render('home', {posts: posts})
+            })
+           
+        })
+
         app.get('/cad', function(req, res){
             res.render('formulario')
         })
         app.post('/add', function(req, res){
-            res.send('texto: '+req.body.titulo+" Conteudo: "+req.body.conteudo )
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then(function(){
+               res.redirect('/')
+                
+            }).catch(function(erro){
+                res.send("Houve um erro: "+ erro)
+            })
         })
+       
+        app.get('/deletar/:id', function(req,res){
+            Post.destroy({where: {'id': req.params.id}}).then(function(){
+                res.send('Postagem deletada com sucesso')
+            }).catch(function(erro){
+                res.send('Esta postagem não existe')
+            })
+        })
+  
 
 app.listen(8081, function(){
     console.log("Servidor Rodando na url http://localhost:8081");
